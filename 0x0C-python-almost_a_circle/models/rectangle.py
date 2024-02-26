f56c1c928b6392436ad79117a72ceeda89e80329
39LPeter <wlone3949@gmail.com>
2024-02-26 04:01:57 -0800

    python almost a circle

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -12,25 +12,35 @@ class Rectangle(Base):
          def width(self)
     return self.__width
     @width.setter
-             def width(self,value)
+             def width(self,value):
+                 self.validate_integer("width",value,False)
                  self.__width=value
      
      @property
          def height(self)
              return self.__height
              @height.setter
-                 def height(self,value)
+                 def height(self,value):
+                     self.validate_integer("height",value,False)
                      self.__heigth=value
      @property
          def x(self)
              return self.__x
              @x.setter
-                 def x(self,value)
+                 def x(self,value):
+                     self.validate_integer("x",value)
                      self.__x=value
      @property
          def y(self)
              return self.__y
              @y.setter
-                 def y(self,value)
+                 def y(self,value):
+                     self.validate_integer("y",value)
                      self.__y=value
-        
+    def validate_integer(self,name,value,eq=True):
+        if type(value)!=int:
+            raise TypeError("{} must be an integer".format(name))
+        if eq and value<=0:
+            raise ValueError("{} must be >0".format(name))
+        elif not eq and value<=0:
+            raise ValueError("{} must be >=0".format(name))
