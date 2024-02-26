1d240c4fd77aa09a679c9ca609e028336bd76064
39LPeter <wlone3949@gmail.com>
2024-02-26 06:16:49 -0800

    base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,18 +1,1 @@
-#!/usr/bin/python3
-""" Module that contains class Base """
-import json
-import csv
-import os.path
-
-
-class Base:
-        """ Class Base """
-            __nb_objects = 0
-
-                def _init_(self, id=None):
-                            """ Initializes instances """
-                                    if id is not None):
-                   
-                                        Base.__nb_objects += 1
-self.id = Base.__nb_objects
 
