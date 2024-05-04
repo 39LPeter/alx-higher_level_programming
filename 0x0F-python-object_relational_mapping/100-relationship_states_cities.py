5429ab51b33a95e5fd83e1b58d3a220dbbdf88e4
39LPeter <wlone3949@gmail.com>
2024-05-03 18:09:30 -0700

    object mapping

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
 """
-    script that changes the name of a State
-    object from the database hbtn_0e_6_usa
+    script that creates the State 'California' with the City 'San Francisco'
+    from the database hbtn_0e_100_usa
 """
 import sys
-from model_state import Base, State
-
 from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy import asc
+from relationship_state import Base, State
+from relationship_city import City
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format
                            (sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     start = sessionmaker()
     start.configure(bind=engine)
     session = start()
-    stmt = session.query(State).filter_by(id=2)
-    for i in stmt:
-        i.name = "New Mexico"
+    stmt = State(name="California", cities=[City(name="San Francisco")])
+    session.add(stmt)
     session.commit()
     session.close()
--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 """
-    Python file similar to model_state.py named model_city.py
-    that contains the class definition of a City
+    Improve the file model_city.py, and save as relationship_city.py
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
-from model_state import Base
+from relationship_state import Base
 
 
 class City(Base):
     """ Class """
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
